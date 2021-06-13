88acf72759bed11ace461a5110cb1fe7d2adcb45
Guillermo Parral <guillermo.parral@mercadolibre.com>
2021-06-13 14:05:13 -0300

    add bootstrap

--- server.js
+++ server.js
@@ -9,11 +9,27 @@ router.listen(8080, () => console.log("Listening on port 8080!"));
 router.use("/dist", express.static("dist"));
 
 router.get("/", (req, res, next) => {
-  const html = renderToString(
+  const Body = renderToString(
     <div id="root-app">
       <App />
       <script src="./dist/index.bundle.js"></script>
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+        crossorigin="anonymous"
+      ></script>
     </div>
   );
+  const html = `
+    <!doctype html>
+    <html>
+      <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+      </head>
+      <body>
+        ${Body}
+      </body>
+    </html>
+  `;
   res.send(html);
 });
--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 const React = require("react");
 
-const App = () => <div>Soy una APP!</div>;
+const App = () => (
+  <>
+    <h1>React 18 Alpha Demo!!!</h1>
+    <h3>Features que se vienen</h3>
+    <ul className="list-group">
+      <li className="list-group-item">Feature 1</li>
+      <li className="list-group-item">Feature 2</li>
+      <li className="list-group-item">Feature 3</li>
+    </ul>
+  </>
+);
 
 module.exports = App;
